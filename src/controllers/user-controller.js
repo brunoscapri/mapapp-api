750c33bd4a111ec6a10d1fa7b34822f0dd407a4e
brunoscapri <bruno.capriglioni@pucpr.edu.br>
2020-03-26 22:42:43 -0300

    adding token when user is created

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,18 +2,32 @@
 
 const repository = require('../repositories/user-repository');
 const md5 = require('md5');
+const auth = require('../services/auth-service');
 
 exports.post = async (req, res) => {
     try {
-        await repository.create({
+        var user = await repository.create({
             name: req.body.name,
             email: req.body.email,
             password: md5(req.body.password) + global.SALT_KEY,
             picture: req.body.picture,
             roles: 'user',
         })
+
+        var token = await auth.generateToken({
+            id: user._id,
+            name: user.name,
+            email: user.email
+        });
+
+
         res.status(200).send({
-            message: "User created."
+            message: "User created.",
+            token: token,
+            data: {
+                email: user.email,
+                name: user.name,
+            }
         });
     } catch (e) {
         res.status(500).send({
--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -4,6 +4,8 @@ const User = mongoose.model('User');
 exports.create = async (data) => {
     var user = new User(data);
     await user.save();
+
+    return user;
 }
 
 
